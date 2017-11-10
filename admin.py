159c1a54cf11774943fe2c8cc4932634aa70e207
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-11-10 14:21:34 -0500

    Mise a jour de l'interface admin

--- admin.py
+++ admin.py
@@ -1,11 +1,24 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 
-from .models import Typequestion, Questionnaire,Question,Province,Personne,Resultat
+from .models import Typequestion, Questionnaire,Questionntp2,Personne,Reponsentp2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+#    admin.site.site_header = 'My admin'
+
+
+    list_display = ('questionno', 'questionen')
+
+    list_filter = ['questionnaire','typequestion']
+
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+
 
 admin.site.register(Typequestion)
 admin.site.register(Questionnaire)
-admin.site.register(Question)
-admin.site.register(Province)
+admin.site.register(Questionntp2, QuestionAdmin)
 admin.site.register(Personne)
-admin.site.register(Resultat)
+admin.site.register(Reponsentp2)
