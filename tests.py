e76e3321db10886df2c31315b0768c4ab139a7a0
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2018-03-19 17:11:10 -0400

    Améliorations cosmétiques

--- tests.py
+++ tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from unittest import TestCase
+from encrypter import Encrypter
 
-# Create your tests here.
+
+class TestCrypto(TestCase):
+    def test_can_generate_two_key_encrypt_and_decrypt(self):
+        e = Encrypter()
+
+        e.save_key('myprivatekey.pem')
+        e.save_public_key('mypublickey.pem')
+
+        public_key = e.read_key('mypublickey.pem')
+        private_key = e.read_key('myprivatekey.pem')
+
+        message = 'un texte'
+        ciphertext = e.encrypt(message, public_key)
+        decripted = e.decrypt(ciphertext, private_key)
+
+        self.assertEqual(decripted, message)
+
